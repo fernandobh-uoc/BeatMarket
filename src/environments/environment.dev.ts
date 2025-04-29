fc9fc9d9126929b2ba1a4a51bd3a90e28ddf5d29
fernandobh <fernandobh@uoc.edu>
2025-04-29 15:51:29 +0200

    Added firebase app

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,16 @@
+//import { FIREBASE_AUTH_TOKEN } from "src/app/core/services/auth/adapters/firebase-auth.adapter";
+//import { FIREBASE_FIRESTORE_TOKENS } from "src/app/core/services/storage/adapters/firebase-firestore.adapter";
+
+export const environment = {
+  production: false,
+  //authToken: FIREBASE_AUTH_TOKEN,
+  //storageTokens: FIREBASE_FIRESTORE_TOKENS,
+  firebase: {
+    apiKey: "AIzaSyAVCQrk9BinjVxO-eEHD4gKwb6UMOahJso",
+    authDomain: "beatmarket-uoc.firebaseapp.com",
+    projectId: "beatmarket-uoc",
+    storageBucket: "beatmarket-uoc.firebasestorage.app",
+    messagingSenderId: "61948068505",
+    appId: "1:61948068505:web:39e9c0e8f5f7d4c56aee08"
+  }
+};
