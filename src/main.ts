2cfa78acb9728b939cd088d3ec4fc5ac7ee53435
fernandobh <fernandobh@uoc.edu>
2025-04-29 18:42:12 +0200

    Setup initial main configuration

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,37 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth, indexedDBLocalPersistence, initializeAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore, initializeFirestore, persistentLocalCache } from '@angular/fire/firestore';
+
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { environment } from './environments/environment.dev';
+import { importProvidersFrom } from '@angular/core';
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { Capacitor } from '@capacitor/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideFirebaseApp(() => {
+      const app = initializeApp(environment.firebase);
+      if (Capacitor.isNativePlatform()) {
+        initializeFirestore(app, {
+          localCache: persistentLocalCache()
+        });
+        initializeAuth(app, {
+          persistence: indexedDBLocalPersistence
+        })
+      }
+      return app;
+    }),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+
+    importProvidersFrom(IonicStorageModule.forRoot())
   ],
 });
