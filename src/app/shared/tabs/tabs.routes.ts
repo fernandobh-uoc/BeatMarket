9346fd54933b241d6dbb6569beb4ca767bb20f83
fernandobh <fernandobh@uoc.edu>
2025-04-30 21:45:32 +0200

    Added shared folder

---
+++ src/app/shared/tabs/tabs.routes.ts
@@ -0,0 +1,29 @@
+import { Routes } from '@angular/router';
+import { TabsPage } from './tabs.page';
+import { authGuard } from 'src/app/core/guards/auth.guard';
+//import { AuthGuard } from '../core/guards/auth.guard';
+
+export const TabsRoutes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'home',
+        loadComponent: () => import('../../features/home/home.page').then(m => m.HomePage)      
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/home',
+        pathMatch: 'full'
+      }
+    ],
+    canMatch: [authGuard],
+    data: { authStatusMustBe: true }
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/home',
+    pathMatch: 'full'
+  }
+]
