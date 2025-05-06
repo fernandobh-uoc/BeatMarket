11e19a5272cc1ba42d50dae5eeefe3640a7607e0
fernandobh <fernandobh@uoc.edu>
2025-05-06 07:02:14 +0200

    Removed logout button

--- src/app/features/home/home.page.ts
+++ src/app/features/home/home.page.ts
@@ -11,20 +11,7 @@ import { ToolbarComponent } from 'src/app/shared/ui/components/toolbar/toolbar.c
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [ToolbarComponent, IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
+  imports: [ToolbarComponent, IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
-  #userRepository = inject(UserRepository);
-  #authService = inject(AuthService);
-  #router = inject(Router);
- 
-  logout = async () => {
-    try {
-      await this.#authService.logout();
-    } catch (error) {
-      console.error(error);
-    }
-
-    this.#router.navigate(['/auth/landing']);
-  }
 }
