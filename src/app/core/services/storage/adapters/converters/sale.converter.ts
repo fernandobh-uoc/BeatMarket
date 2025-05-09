d33d7ca604065ce9ab2a8955f817e01e1f333d4d
fernandobh <fernandobh@uoc.edu>
2025-05-09 08:07:08 +0200

    Added sale converter

---
+++ src/app/core/services/storage/adapters/converters/sale.converter.ts
@@ -0,0 +1,90 @@
+import { DocumentData, FirestoreDataConverter, QueryDocumentSnapshot, serverTimestamp, SnapshotOptions, Timestamp, WithFieldValue } from "firebase/firestore";
+import { Sale, SaleModel, SalePostData, SaleUserData } from "src/app/core/domain/models/sale.model";
+import { isFieldValue, isFirestoreTimestamp, isValidDateInput } from "./utils/converter.utils";
+import { FirestoreSaleRepository } from "src/app/core/domain/repositories/firestore/firestore.sale.repository";
+import { ArticleCondition } from "src/app/core/domain/models/article.model";
+
+export interface SaleFirestoreModel {
+  postData: {
+    postId: string;
+    title: string;
+    articleCondition: string;
+    price: number;
+  };
+  buyerData: {
+    userId: string;
+    username: string;
+  };
+  sellerData: {
+    userId: string;
+    username: string;
+  };
+  saleDate: Timestamp;
+  createdAt: Timestamp,
+  updatedAt: Timestamp;
+}
+
+export class SaleConverter implements FirestoreDataConverter<SaleModel, SaleFirestoreModel> {
+  toFirestore(sale: WithFieldValue<SaleModel>): WithFieldValue<SaleFirestoreModel> {
+    return {
+      postData: {
+        postId: (<SalePostData>sale.postData).postId,
+        title: (<SalePostData>sale.postData).title,
+        articleCondition: (<SalePostData>sale.postData).articleCondition,
+        price: (<SalePostData>sale.postData).price,
+      },
+      buyerData: {
+        userId: (<SaleUserData>sale.buyerData).userId,
+        username: (<SaleUserData>sale.buyerData).username,
+      },
+      sellerData: {
+        userId: (<SaleUserData>sale.sellerData).userId,
+        username: (<SaleUserData>sale.sellerData).username,
+      },
+      saleDate: isValidDateInput(sale.saleDate)
+        ? Timestamp.fromDate(new Date(sale.saleDate))
+        : isFieldValue(sale.saleDate)
+          ? sale.saleDate
+          : serverTimestamp(),
+      createdAt: isValidDateInput(sale.createdAt)
+        ? Timestamp.fromDate(new Date(sale.createdAt))
+        : isFieldValue(sale.createdAt)
+          ? sale.createdAt
+          : serverTimestamp(),
+      updatedAt: isValidDateInput(sale.updatedAt)
+        ? Timestamp.fromDate(new Date(sale.updatedAt))
+        : isFieldValue(sale.updatedAt)
+          ? sale.updatedAt
+          : serverTimestamp()
+    }
+  }
+
+  fromFirestore(snapshot: QueryDocumentSnapshot<SaleFirestoreModel>, options?: SnapshotOptions): SaleModel {
+    const data = snapshot.data(options);
+
+    return Sale.Build({
+      _id: snapshot.id,
+      postData: {
+        postId: data.postData.postId,
+        title: data.postData.title,
+        articleCondition: data.postData.articleCondition as ArticleCondition,
+        price: data.postData.price,
+      },
+      buyerData: {
+        userId: data.buyerData.userId,
+        username: data.buyerData.username,
+      },
+      sellerData: {
+        userId: data.sellerData.userId,
+        username: data.sellerData.username,
+      },
+      saleDate: data.saleDate.toDate(),
+      createdAt: isFirestoreTimestamp(data.createdAt) 
+        ? data.createdAt.toDate() 
+        : null,
+      updatedAt: isFirestoreTimestamp(data.updatedAt) 
+        ? data.updatedAt.toDate() 
+        : null,
+    });
+  }
+}
