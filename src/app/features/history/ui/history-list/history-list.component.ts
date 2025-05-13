854e9cc1959461b7ed557eb7c06b9801f14e4b7b
fernandobh <fernandobh@uoc.edu>
2025-05-13 14:26:50 +0200

    Added link to user detail in history

--- src/app/features/history/ui/history-list/history-list.component.ts
+++ src/app/features/history/ui/history-list/history-list.component.ts
@@ -3,12 +3,13 @@ import { Sale } from 'src/app/core/domain/models/sale.model';
 import { IonText, IonList, IonItem, IonLabel, IonThumbnail, IonAvatar } from "@ionic/angular/standalone";
 import { FormatCurrencyPipe } from "../../../../shared/utils/pipes/format-currency.pipe";
 import { DatePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-history-list',
   templateUrl: './history-list.component.html',
   styleUrls: ['./history-list.component.scss'],
-  imports: [DatePipe, IonAvatar, IonLabel, IonItem, IonList, IonText, IonThumbnail, FormatCurrencyPipe]
+  imports: [RouterLink, DatePipe, IonAvatar, IonLabel, IonItem, IonList, IonText, IonThumbnail, FormatCurrencyPipe]
 })
 export class HistoryListComponent  implements OnInit {
   sales = input<Sale[] | null>(null);
