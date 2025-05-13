629c4994ceacdffc84a85e89fcbec11012bcfe93
fernandobh <fernandobh@uoc.edu>
2025-05-13 09:13:55 +0200

    Modified resolver so that it returns empty array instead of null

--- src/app/features/home/utils/resolvers/home.resolver.ts
+++ src/app/features/home/utils/resolvers/home.resolver.ts
@@ -4,12 +4,12 @@ import { HomeService } from '../../data-access/home.service';
 import { Post } from 'src/app/core/domain/models/post.model';
 import { Observable } from 'rxjs';
 
-export const latestPostsResolver: ResolveFn<Promise<Post[] | null>> = async (route, state) => {
+export const latestPostsResolver: ResolveFn<Promise<Post[]>> = async (route, state) => {
   const homeService = inject(HomeService);
-  return await homeService.getLatestPosts({ limit: 10, orderBy: { field: 'createdAt', direction: 'desc' } });
+  return await homeService.getLatestPosts({ limit: 10, orderBy: { field: 'createdAt', direction: 'desc' } }) ?? [];
 };
 
-export const recommendedPostsResolver: ResolveFn<Promise<Post[] | null>> = async (route, state) => {
+export const recommendedPostsResolver: ResolveFn<Promise<Post[]>> = async (route, state) => {
   const homeService = inject(HomeService);
-  return await homeService.getRecommendedPosts({ limit: 10 });
+  return await homeService.getRecommendedPosts({ limit: 10 }) ?? [];
 };
