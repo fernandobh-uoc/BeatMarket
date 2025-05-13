c100d1690958fea6bcb3fd32f36d283bcf2426f6
fernandobh <fernandobh@uoc.edu>
2025-05-13 08:37:57 +0200

    Removed an import

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { APP_INITIALIZER, inject, provideAppInitializer } from '@angular/core';
+import { inject, provideAppInitializer } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
