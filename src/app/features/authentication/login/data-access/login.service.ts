a3e93a9a35b03c83efa6638d67b2dbf41f3cc3e8
fernandobh <fernandobh@uoc.edu>
2025-05-15 14:02:43 +0200

    Updated login service to update the fcm token

--- src/app/features/authentication/login/data-access/login.service.ts
+++ src/app/features/authentication/login/data-access/login.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable, signal } from '@angular/core';
 import { UserModel } from 'src/app/core/domain/models/user.model';
 import { UserRepository } from 'src/app/core/domain/repositories/user.repository';
 import { AuthService } from 'src/app/core/services/auth/auth.service';
+import { LocalStorageService } from 'src/app/core/storage/local-storage.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,28 +10,31 @@ import { AuthService } from 'src/app/core/services/auth/auth.service';
 export class LoginService {
   #authService = inject(AuthService);
   #userRepository = inject(UserRepository);
+  #cache = inject(LocalStorageService);
 
   public errorMessage = signal<string>(''); // Allow to be changed from login page on control focus
 
-  #isEmail(input: string): boolean {
-    return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(input);
-  }
-
   async login({ emailOrUsername, password }: { emailOrUsername: string; password: string }): Promise<void> {
-    let email: string | null = emailOrUsername;
+    let email: string = emailOrUsername;
     let user: UserModel | null = null; 
+    let fcmToken: string | null = await this.#cache.get<string | null>('fcmToken');
     
     try {
-      if (!emailOrUsername.includes('@') || !this.#isEmail(emailOrUsername)) {
+      user = await this.#userRepository.getUserByEmail(emailOrUsername);
+      if (!user) {
         user = await this.#userRepository.getUserByUsername(emailOrUsername);
         if (!user) {
           this.errorMessage.set('Ese usuario no existe.');
           return;
         }
-        email = emailOrUsername;
+        email = user.email;
       }
 
       await this.#authService.login({ method: 'email', credentials: { email, password }});
+      await this.#userRepository.updateUser({
+        _id: user._id, 
+        fcmToken: fcmToken
+      })
     } catch (loginError: any) {
       this.errorMessage.set(loginError);
     } 
