10c90b13bf930f42a21111101d18a466211bf592
fernandobh <fernandobh@uoc.edu>
2025-05-21 02:33:41 +0200

    Added a big cart icon in the background when cart is empty

--- src/app/features/cart/cart.page.ts
+++ src/app/features/cart/cart.page.ts
@@ -1,20 +1,20 @@
 import { Component, computed, effect, inject, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonText, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonText, IonButton, IonIcon } from '@ionic/angular/standalone';
 import { ToolbarComponent } from 'src/app/shared/ui/components/toolbar/toolbar.component';
 import { CartItemsListComponent } from './ui/cart-items-list/cart-items-list.component';
 import { CartService } from './data-access/cart.service';
 import { CartItemModel } from 'src/app/core/domain/models/cart.model';
 import { FormatCurrencyPipe } from "../../shared/utils/pipes/format-currency.pipe";
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.page.html',
   styleUrls: ['./cart.page.scss'],
   standalone: true,
-  imports: [RouterLink, IonButton, IonText, CartItemsListComponent, ToolbarComponent, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, FormatCurrencyPipe]
+  imports: [IonIcon, RouterLink, IonButton, IonText, CartItemsListComponent, ToolbarComponent, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, FormatCurrencyPipe]
 })
 export class CartPage {
   private cartService = inject(CartService);
