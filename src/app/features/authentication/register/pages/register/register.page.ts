b43f06ae9441e0e44f00c7c58c95319fdac73e97
fernandobh <fernandobh@uoc.edu>
2025-05-28 18:38:57 +0200

    Forgot to change the form default step back to 1

--- src/app/features/authentication/register/pages/register/register.page.ts
+++ src/app/features/authentication/register/pages/register/register.page.ts
@@ -35,7 +35,7 @@ export class RegisterPage implements ViewDidLeave {
 
   registerFormComponent = viewChild(RegisterFormComponent);
 
-  step = signal<number>(3);
+  step = signal<number>(1);
   totalSteps = 4;
   progressBarValue = computed(() => (this.step() / this.totalSteps));
 
