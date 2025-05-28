02301cf2e3fdcb892517a125cc4d086712e342e1
fernandobh <fernandobh@uoc.edu>
2025-05-28 07:31:25 +0200

    Added zipcode API key

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -7,5 +7,8 @@ export const environment = {
     storageBucket: "beatmarket-uoc.firebasestorage.app",
     messagingSenderId: "61948068505",
     appId: "1:61948068505:web:39e9c0e8f5f7d4c56aee08"
+  },
+  zipcodeStack: {
+    apiKey: "zip_live_OjBu0DPDlMXGNcC1uf6sJwtMSsCCaBGu4j0LFTjN"
   }
 };
