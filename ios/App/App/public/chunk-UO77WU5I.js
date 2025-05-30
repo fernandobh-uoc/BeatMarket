016513eb9f358912b83a63978d297e6fd97625d4
fernandobh <fernandobh@uoc.edu>
2025-05-30 07:32:48 +0200

    Updated builds

--- ios/App/App/public/chunk-UO77WU5I.js
+++
@@ -1,1 +0,0 @@
-import{f as a}from"./chunk-FIPIWZQS.js";var o,e=a(()=>{o=[{path:"",redirectTo:"payment",pathMatch:"full"},{path:"payment",loadComponent:()=>import("./chunk-NPOWT6OW.js").then(t=>t.PaymentPage)},{path:"splash",loadComponent:()=>import("./chunk-VWH2AX7T.js").then(t=>t.SplashPage)}]});e();export{o as CheckoutRoutes};
